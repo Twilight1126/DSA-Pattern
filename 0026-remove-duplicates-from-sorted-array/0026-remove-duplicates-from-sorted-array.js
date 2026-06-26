a568bf25bd203ba27e8a30d89447fcef30b72584
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-06-26 17:30:28 +0530

    Time: 0 ms (100%), Space: 58.4 MB (26.91%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,13 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-    let k = 0;
+    let j = 0;
     for (let i = 1; i < nums.length; i++) {
-        if (nums[i] !== nums[k]) {
-            k++;
-            nums[k] = nums[i];
+        if (nums[i] !== nums[j]) {
+            j++;
+            nums[j] = nums[i];
         }
     }
-    return k + 1;
+    return j + 1;
 
 };
