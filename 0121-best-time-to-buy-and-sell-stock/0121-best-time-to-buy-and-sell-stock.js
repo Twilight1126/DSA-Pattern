6dc4f034128e19c686940adabd3bf7df92abea49
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-06-27 21:41:02 +0530

    Time: 3 ms (58.52%), Space: 65.1 MB (60.2%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -4,15 +4,16 @@
  */
 var maxProfit = function (prices) {
     let minprice = prices[0];
-    let maxprofit = 0;
+    let maxValue = 0;
 
     for (let i = 1; i < prices.length; i++) {
         if (prices[i] < minprice) {
             minprice = prices[i];
         }
-        else if ((prices[i] - minprice) > maxprofit) {
-            maxprofit = prices[i] - minprice;
+       else {
+            let profit = prices[i] - minprice;
+            if (profit > maxValue) maxValue = profit;    
         }
     }
-    return maxprofit;
+    return maxValue;
 }; 
