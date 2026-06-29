cedaa7f4ae6fe3532a815120058350ad0aafcb65
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-06-29 18:15:21 +0530

    Time: 4 ms (53.78%), Space: 56.2 MB (74.52%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -4,26 +4,26 @@
  */
 var majorityElement = function (nums) {
     let store = new Map();
-    for (let i = 0; i < nums.length; i++) {
-        if (store.has(nums[i])) {
+
+    for(let  i = 0 ; i < nums.length; i++){
+        if(store.has(nums[i])){
             let count = store.get(nums[i]);
             count++;
-            store.set(nums[i], count);
+            store.set(nums[i],count);
         }
-        else {
+        else{
             let startingcount = 1;
-            store.set(nums[i], startingcount);
+            store.set(nums[i],startingcount);
         }
     }
     let highestvalue = 0;
     let majorityvalue = -1;
-    for (let [key, value] of store.entries()) {
-        if (value > highestvalue) {
+    for(let [key,value] of store.entries()){
+        if(value > highestvalue){
             highestvalue = value;
             majorityvalue = key;
-
         }
     }
     return majorityvalue;
-
+   
 };
