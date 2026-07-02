ae80562f91fd4d95e48dd5867ecfe7f0ba06bfcc
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-02 13:25:07 +0530

    Time: 6 ms (95.69%), Space: 56 MB (86.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.js
+++ 0383-ransom-note/0383-ransom-note.js
@@ -3,22 +3,23 @@
  * @param {string} magazine
  * @return {boolean}
  */
-var canConstruct = function(ransomNote, magazine) {
+var canConstruct = function (ransomNote, magazine) {
     let store = new Array(26).fill(0);
     // first for loop to  iterate over magazine.length and update that place by index++
-    for(let  i = 0 ; i  < magazine.length; i++){
+    for (let i = 0; i < magazine.length; i++) {
         let index = magazine[i].charCodeAt(0) - 97;
-        store[index]++;       
+        store[index]++;
     }
     // second loop to  iterate over magazine.length and update that place by index--
-     for(let i = 0; i < ransomNote.length; i++){
-         let index = ransomNote[i].charCodeAt(0) - 97;
-         store[index]--;
-         if(store[index] < 0){
+    for (let i = 0; i < ransomNote.length; i++) {
+        let index = ransomNote[i].charCodeAt(0) - 97;
+        store[index]--;
+        
+        if (store[index] < 0) {
             return false;
-         }
+        }
 
-     }
-     return true;
-    
+    }
+    return true;
+
 };
