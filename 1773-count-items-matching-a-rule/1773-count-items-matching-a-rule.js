92afdb524fcbeadf5c8e76e0052d1fde54cfb29c
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-03 14:48:10 +0530

    Time: 0 ms (100%), Space: 59.6 MB (61.47%) - LeetHub

---
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+var countMatches = function (items, ruleKey, ruleValue) {
+    let count = 0;
+    let index;
+    if (ruleKey === 'type') index = 0;
+    else if (ruleKey === 'color') index = 1;
+    else index = 2;
+    for (let i = 0; i < items.length; i++) {
+        if (items[i][index] === ruleValue) {
+            count++;
+        }
+    }
+    return count;
+
+};
