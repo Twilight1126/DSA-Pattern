1bef5b52cb8f4ad84cfe71934191eda4766775c6
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-04 14:09:23 +0530

    Time: 4 ms (73.12%), Space: 60.4 MB (88.19%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.js
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -2,14 +2,14 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
+var romanToInt = function (s) {
     let total = 0;
-    let charactervalues = {'I': 1, 'V':5, 'X': 10, 'L': 50,'C': 100, 'D': 500, 'M': 1000};
-    for(let i = 0; i < s.length; i++){
-        if(charactervalues[s[i]] < charactervalues[s[i+1]])
-          total = total - charactervalues[s[i]]; 
-          else
-          total = total + charactervalues[s[i]]; 
+    let charactervalues = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 };
+    for (let i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && charactervalues[s[i]] < charactervalues[s[i + 1]])
+            total = total - charactervalues[s[i]];
+        else
+            total = total + charactervalues[s[i]];
     }
     return total;
 
