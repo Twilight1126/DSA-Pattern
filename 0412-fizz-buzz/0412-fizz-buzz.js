f3af5ad6e10af54334fa4827976c6a3cb52ac227
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-04 15:54:28 +0530

    Time: 1 ms (56.27%), Space: 58.8 MB (14.84%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.js
+++ 0412-fizz-buzz/0412-fizz-buzz.js
@@ -2,20 +2,20 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
+var fizzBuzz = function (n) {
     let result = [];
-    for(let  i = 1; i <= n; i ++){
-        if(i % 3 === 0 && i % 5 ===0 ){
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
             result.push('FizzBuzz');
         }
-        else if(i % 3 === 0){
+        else if (i % 3 === 0) {
             result.push('Fizz');
         }
-        else if(i % 5 === 0){
+        else if (i % 5 === 0) {
             result.push('Buzz');
         }
         else
-        result.push(`${i}`);
+            result.push(String(i));
 
     }
     return result;
