1f3416e7bc9e94ca08aadc1bcc098c10ea7831b7
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-06 21:46:08 +0530

    Time: 5 ms (61.24%), Space: 62.5 MB (48.8%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    let k = nums.length - 1;
+    let result = new Array(nums.length);
+
+    while ( left <= right) {
+        let leftsq = Math.pow(nums[left], 2);
+        let rightsq = Math.pow(nums[right], 2);
+        if (leftsq >= rightsq) {
+            result[k] = leftsq;
+            left++;
+            k--;
+        }
+        else {
+            result[k] = rightsq;
+            right--;
+            k--
+        }
+
+    }
+    return result;
+};
