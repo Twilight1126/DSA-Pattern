8267525cd5d15f83d3f1efa20eb36bd187779d21
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-09 14:14:37 +0530

    Time: 7 ms (56.05%), Space: 58.7 MB (47.88%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let seen = new Set();
+    let left = 0;
+    let Maxlen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
+
+        }
+        seen.add(s[right]);
+        Maxlen = Math.max(Maxlen, right - left + 1);
+    }
+    return Maxlen;
+};
