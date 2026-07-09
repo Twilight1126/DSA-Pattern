fd1488adea63a9e288f65313dfc24d07bebb6ede
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-09 18:35:24 +0530

    Time: 8 ms (46.07%), Space: 59.6 MB (29.13%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -2,20 +2,23 @@
  * @param {string} s
  * @return {number}
  */
+// sloving this solution using set and map
+
 var lengthOfLongestSubstring = function (s) {
     let seen = new Set();
     let left = 0;
-    let maxlen = 0;
+    let Maxlen = 0;
 
-    for(let right = 0; right < s.length; right++){
+    for (let right = 0; right < s.length; right++) {
         const char = s[right];
-        while(seen.has(char)){
+        while (seen.has(char)) {
             seen.delete(s[left]);
             left++;
         }
-        seen.add(char,right);
-        maxlen = Math.max(maxlen,right - left + 1 );
+        seen.add(char, right);
+        Maxlen = Math.max(Maxlen, right - left + 1)
     }
-    return maxlen;
-
+    
+    return Maxlen;
 };
+
