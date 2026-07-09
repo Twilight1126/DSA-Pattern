1611804c89606594197cc3ccb92cc9e3700a6caa
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-09 14:28:28 +0530

    Time: 4 ms (87.29%), Space: 57.2 MB (77.52%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let seen = new Set();
+    let seen = new Map();
     let left = 0;
     let Maxlen = 0;
 
     for (let right = 0; right < s.length; right++) {
-        while (seen.has(s[right])) {
-            seen.delete(s[left]);
-            left++;
-
+        const char = s[right];
+        if(seen.has(char) && seen.get(char) >= left){
+            left = seen.get(char) + 1;
         }
-        seen.add(s[right]);
+        seen.set(char,right);
         Maxlen = Math.max(Maxlen, right - left + 1);
     }
     return Maxlen;
