840629be4a6db7d3fceb4c745a3dc3c7fce511a7
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-12 22:39:43 +0530

    Time: 1 ms (88.08%), Space: 56.9 MB (10.41%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,7 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-    let sum = 0;
+    let seen = new Map();
+    for(let i = 0; i < nums.length; i++){
+        let compliment = target - nums[i];
+        if(seen.has(compliment)){
+            return [seen.get(compliment),i];
+        }
+        seen.set(nums[i],i);
+    }   
+};
+
+
+/*
+let sum = 0;
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
             sum = nums[i] + nums[j];
@@ -13,6 +25,4 @@ var twoSum = function (nums, target) {
             }
         }
     }
-
-
-};
+    */
