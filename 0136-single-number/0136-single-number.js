4b2d29b1b7e36b1a460af3fd1f041719ecf8d9e8
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-14 14:44:32 +0530

    Time: 3 ms (47.05%), Space: 58.4 MB (27.43%) - LeetHub

---
+++ 0136-single-number/0136-single-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+    let seen = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])){
+            seen.delete(nums[i]);
+        }
+        else {
+        seen.add(nums[i]);
+        }
+    }
+    return [...seen][0];
+};
