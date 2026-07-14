d4b9a9c45b9c122debe36280e26f369c90cb6ce9
Chirag <113207664+Twilight1126@users.noreply.github.com>
2026-07-14 19:15:34 +0530

    Time: 0 ms (100%), Space: 53.7 MB (45.33%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,7 +3,7 @@
  * @return {number[]}
  */
 var plusOne = function (digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
+    for (let i = digits.length-1; i >= 0; i--) {
         if (digits[i] === 9) {
             digits[i] = 0;
         }
@@ -11,10 +11,9 @@ var plusOne = function (digits) {
             digits[i]++;
             return digits;
         }
-    }
 
+    }
+    // edge case when input is [9,9,9] to make add zero at last we have unshift so the result will be [1,1,1]
     digits.unshift(1);
     return digits;
-
-
 };
